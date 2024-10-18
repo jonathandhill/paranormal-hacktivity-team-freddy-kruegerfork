6ea3ffb3f0c73c9458dc03765c365704ee0844a0
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-10-18 14:03:38 +0100

    added more

--- my-app/src/app/pong/page.js
+++ my-app/src/app/pong/page.js
@@ -6,45 +6,105 @@ export default function Pong() {
     // Create canvas
     const canvas = document.getElementById("game");
     const context = canvas.getContext("2d");
+
     // Define grid, paddleheight, maxPadY
     const grid = 15;
-    const paddleheight = grid * 5;
-    const maxPadY = canvas.height - paddleheight - grid;
+    const paddleHeight = grid * 5;
+
+    // Lowest Y for paddle:
+    const maxPadY = canvas.height - paddleHeight - grid;
 
     // hardcode paddle&ball speed
+    let paddleSpeed = 6;
+    let ballSpeed = 4
 
     // define left, right (keys: x, y, wid, hei, vel) & ball objects (x, y, velX, velY, reset)
+    const leftPaddle = {
+        x: grid * 2,
+        y: canvas.height / 2 - paddleHeight /2, // middle
+        wid: grid,
+        hei: paddleHeight,
+        vel: 0,
+    }
 
-    // define collision function
+    const rightPaddle = {
+        x: canvas.width - grid * 3,
+        y: canvas.height / 2 - paddleHeight / 2, // middle
+        wid: grid,
+        hei: paddleHeight,
+        vel: 0,
+    }
+
+    const ball = {
+        x: canvas.width - grid * 3,
+        y: canvas.height / 2 - paddleHeight / 2, // middle
+        wid: grid,
+        hei: grid,
+        velX: ballSpeed,
+        velY: ballSpeed,
+        reset: false,
+    }
+
+    // define collision function. collision detection algorithm for axis-aligned bounding boxes (AABB)
+    // true if the objects are colliding, false otherwise.
+    function collision(obj1, obj2) {
+        return obj1.x < obj2.x + obj2.width && // Checks if the left edge of obj1 is to the left of the right edge of obj2.
+               obj1.x + obj1.width > obj2.x && //Checks if the right edge of obj1 is to the right of the left edge of obj2.
+               obj1.y < obj2.y + obj2.height && //Checks if the top edge of obj1 is above the bottom edge of obj2.
+               obj1.y + obj1.height > obj2.y //Checks if the bottom edge of obj1 is below the top edge of obj2.
+    }
 
     // define game loop
-    // start animation with requestAnimationFrame
-    // clear pixels
+    function gameLoop() {
+        // start animation with requestAnimationFrame (tells the browser you wish to perform an animation)
+        requestAnimationFrame(gameLoop);
 
-    // move paddles by velocity
+        // clear pixels (context.clearRect(x, y, width, height))
+        context.clearRect(0,0,canvas.width,canvas.height);
 
-    // prevent paddles going through walls
+        // move paddles by velocity
+        leftPaddle.y += leftPaddle.vel;
+        rightPaddle.y += rightPaddle.vel;
 
-    // draw paddles
+        // prevent paddles going through walls
+        // if y val is smaller than grid - move back to grid
+        if (leftPaddle.y < grid) {
+            leftPaddle.y = grid;
+        }
+        else if (leftPaddle.y > maxPadY) {
+            leftPaddle.y = maxPadY;
+        }
 
-    // move ball by its velocity
+        if (rightPaddle.y < grid) {
+            rightPaddle.y = grid;
+        }
+        else if (rightPaddle.y > maxPadY) {
+            rightPaddle.y = maxPadY;
+        }
+        // draw paddles (context.fillRect(x, y, width, height))
+        context.fillStyle = 'white';
+        context.fillRect()
 
-    // prevent balls going through wall by changing velocity
+        // move ball by its velocity
 
-    // reset ball if goes past paddle & reset is false
-        //give players time before start again
+        // prevent balls going through wall by changing velocity
+
+        // reset ball if goes past paddle & reset is false
+            //give players time before start again
 
 
-    // check if ball collides with paddle
+        // check if ball collides with paddle
 
-    //DRAW - ball, walls, middle line
-
+        //DRAW - ball, walls, middle line
+    }
     //listen to keyboard events
     // 4 keys
 
     // listen for keys being released: stop
 
-    //START GAME, call function
+    //START GAME, call 
+    requestAnimationFrame(gameLoop);
+
 
 
 
