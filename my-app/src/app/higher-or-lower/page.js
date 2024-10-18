a23356c37265ce57ef8e60b7e24389545b611b07
Âdil <therealadil@icloud.com>
2024-10-18 11:29:41 +0100

    FIX BRANCHING ISSUES

--- my-app/src/app/higher-or-lower/page.js
+++ my-app/src/app/higher-or-lower/page.js
@@ -4,31 +4,30 @@ import './page.module.css';
 import Image from 'next/image';
 
 
-
-
-
-
-
-
 export default function HigherOrLower() {
 
-    // array of cards (per suit)
+// array of cards (per suit)
 let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 //  random card
 let card = cards[Math.floor(Math.random() * cards.length)]
 
-// let playerChoice = prompt("Higher or lower")
+let playerChoice = prompt("Higher or lower")
 
 let newCard = cards[Math.floor(Math.random() * cards.length)]
 
-if (newCard > card && playerChoice === "higher") {
+if (newCard > card && playerChoice === "higher" || newCard < card && playerChoice === "lower") {
   console.log("You win!")
+} else {
+  console.log("You lose!")
 }
+
 
   return (
     <>
         <h1>Higher or Lower</h1>
+
+
     </>
   );
 }
