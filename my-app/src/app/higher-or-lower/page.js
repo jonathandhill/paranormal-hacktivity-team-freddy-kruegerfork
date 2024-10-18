bc4107e2108cea53d69e7ae09741e4e16b4a37bf
Âdil <therealadil@icloud.com>
2024-10-18 12:39:46 +0100

    update higher lower logic

--- my-app/src/app/higher-or-lower/page.js
+++ my-app/src/app/higher-or-lower/page.js
@@ -1,7 +1,8 @@
-
+"use client"
 import Link from 'next/link';
 import './page.module.css';
 import Image from 'next/image';
+import { useState } from 'react';
 
 
 export default function HigherOrLower() {
@@ -9,26 +10,41 @@ export default function HigherOrLower() {
 // array of cards (per suit)
 let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-//  random card
-let card = cards[Math.floor(Math.random() * cards.length)]
+const [playerChoice, setPlayerChoice] = useState("null")
 
-let playerChoice = prompt("Higher or lower")
+function gameRound() {
 
-let newCard = cards[Math.floor(Math.random() * cards.length)]
+  let card = cards[Math.floor(Math.random() * cards.length)]
 
-if (newCard > card && playerChoice === "higher" || newCard < card && playerChoice === "lower") {
-  console.log("You win!")
-} else {
-  console.log("You lose!")
+
+  // pick next random card
+  let newCard = cards[Math.floor(Math.random() * cards.length)]
+
+  // if (newCard === card) {
+  //   console.log("It's a tie! try again")
+  // } else
+
+  console.log(newCard)
+
+  if (newCard > card && playerChoice === "higher" || newCard < card && playerChoice === "lower") {
+    console.log("You win!")
+  } else {
+    console.log("You lose!")
+  }
+
 }
 
 
   return (
     <>
         <h1>Higher or Lower</h1>
-
+        <button onClick={gameRound}>Play</button>
+        <button onClick={() => { setPlayerChoice("higher")  }}>Higher</button>
+        <button onClick={() => { setPlayerChoice("lower")  }}>Lower</button>
 
     </>
   );
 }
+
+
 
